c1a5cb9681ca24030d476ea1a8c92d8d8aeb04a1
Dave <davidajberner@gmail.com>
2015-03-26 20:01:36 +1100

    Vanilla Tabs starting point

---
+++ tabs/js/vanilla-ui.tabs.js
@@ -0,0 +1,60 @@
+(function(){
+
+    /**
+    * wshVanillaTabs Module
+    */
+
+    var wshVanillaTabs = function(){
+
+        var self = this,
+                settings = {
+                    activeTab : null,
+                };
+
+        var domElements = {
+            tabNav          : document.querySelectorAll('[ui-tab-nav]'),
+            tab             : document.querySelectorAll('[ui-tab]')
+        };
+
+        this.init = function(){
+            // Other logic here
+            console.log('What!');
+            this.bindEvents();
+        };
+
+        this.bindEvents = function() {
+            document.body.onmouseup = function (event) {
+                var target = event.target || event.toElement;
+                if (target.hasAttribute("ui-tab-nav")){ self.showTab() };
+            };
+        };
+
+        this.showTab = function() {
+
+             var targetTabId = event.target.attributes['ui-tab-nav'].value;
+             var targetTabNav = document.querySelectorAll('[ui-tab-nav="' + targetTabId + '"]')[0]
+             var targetTab = document.querySelectorAll('[ui-tab="' + targetTabId + '"]')[0];
+
+             this.hideTabs();
+
+             settings.activeTab = targetTab;
+
+             targetTabNav.className += " tab-nav__item--current";
+             targetTab.className += " tab--current";
+
+        };
+
+        this.hideTabs = function(){
+
+            for (var i = 0, max = domElements.tab.length; i < max; i++) {
+                domElements.tab[i].className = domElements.tab[i].className.replace( /(?:^|\s)tab--current(?!\S)/g , '' );
+
+                domElements.tabNav[i].className = domElements.tabNav[i].className.replace( /(?:^|\s)tab-nav__item--current(?!\S)/g , '' );
+            };
+        };
+    };
+
+    var tabs = new wshVanillaTabs();
+    tabs.init();
+
+})();
