d6d6138666c0a818f5534a2f73fabe8ae55a0534
Dave <davidajberner@gmail.com>
2015-03-26 21:07:14 +1100

    Dropdown starting point

---
+++ dropdown/js/vanilla-ui.dropdown.js
@@ -0,0 +1,63 @@
+(function(){
+
+    /**
+    * wshVanillaDropdowns Module
+    */
+
+    var wshVanillaDropdown = function(){
+
+        var self = this,
+                settings = {
+                    activeDropdown : null,
+                };
+
+        var domElements = {
+            dropdownTrigger      : document.querySelectorAll('[ui-dropdown-trigger]'),
+            dropdown             : document.querySelectorAll('[ui-dropdown]')
+        };
+
+        this.init = function(){
+            // Other logic here
+            this.bindEvents();
+        };
+
+        this.bindEvents = function() {
+            document.body.onmouseup = function (event) {
+                var target = event.target || event.toElement;
+                if (target.hasAttribute("ui-dropdown-trigger")){
+                    self.showDropdown();
+                } else {
+                    self.hideDropdowns();
+                };
+            };
+        };
+
+        this.showDropdown = function() {
+
+             var targetDropdownId = event.target.attributes['ui-dropdown-trigger'].value;
+             var targetDropdownTrigger = document.querySelectorAll('[ui-dropdown-trigger="' + targetDropdownId + '"]')[0]
+             var targetDropdown = document.querySelectorAll('[ui-dropdown="' + targetDropdownId + '"]')[0];
+
+             this.hideDropdowns();
+
+             settings.activeDropdown = targetDropdown;
+
+             targetDropdownTrigger.className += " dropdown-trigger--current";
+             targetDropdown.className += " dropdown--is-open";
+
+        };
+
+        this.hideDropdowns = function(){
+
+            for (var i = 0, max = domElements.dropdown.length; i < max; i++) {
+                domElements.dropdown[i].className = domElements.dropdown[i].className.replace( /(?:^|\s)dropdown--is-open(?!\S)/g , '' );
+
+                domElements.dropdownTrigger[i].className = domElements.dropdownTrigger[i].className.replace( /(?:^|\s)dropdown-trigger--current(?!\S)/g , '' );
+            };
+        };
+    };
+
+    var dropdown = new wshVanillaDropdown();
+    dropdown.init();
+
+})();
