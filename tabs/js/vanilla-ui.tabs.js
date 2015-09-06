5bc0a5f1d848073cb2ea0845fbe02b76c23c3435
Dave <davidajberner@gmail.com>
2015-09-06 11:50:26 +1000

    Adding aria roles, removed ui attributes

--- tabs/js/vanilla-ui.tabs.js
+++ tabs/js/vanilla-ui.tabs.js
@@ -6,55 +6,89 @@
     * wshVanillaTabs Module
     */
 
-    var wshVanillaTabs = function(){
+    var wshVanillaTabs = function(pActiveTabNav, pActiveTabPane, pTabsHook, pActiveTabNavClassName, pActiveTabPaneClassName){
 
-        var self = this,
-                settings = {
-                    activeTab : null,
+        var self = this;
+        var settings = {
+                    activeTabNav  : document.getElementById(pActiveTabNav),
+                    activeTabPane : document.getElementById(pActiveTabPane),
+                    tabsHook: pTabsHook || 'js-tab-nav', 
+                    activeTabNavClassName: pActiveTabNavClassName || 'tab-nav__item--current',
+                    activeTabPaneClassName: pActiveTabPaneClassName || 'tab-pane--current',
                 };
-
-        var domElements = {
-            tabNav          : document.querySelectorAll('[ui-tab-nav]'),
-            tab             : document.querySelectorAll('[ui-tab]')
-        };
-
+        var ENTER_KEY 		= 13;
+        var LEFT_ARROW_KEY  = 37;
+        var RIGHT_ARROW_KEY = 39;
+       
         this.init = function(){
             this.bindEvents();
+            this.showTab(); 
         };
 
         this.bindEvents = function() {
+            
             document.body.onmouseup = function (event) {
-                var target = event.target || event.toElement;
-                if (target.hasAttribute('ui-tab-nav')){ self.showTab(); }
+                self.checkTarget(event);
+            };
+
+            document.body.onkeydown = function(event){
+            	if(event.keyCode == ENTER_KEY || event.which == ENTER_KEY) {
+            		self.checkTarget(event);
+            	}
+            	if(event.keyCode == LEFT_ARROW_KEY || event.which == LEFT_ARROW_KEY) {
+            		console.log('lefty!');
+            	}
+            	if(event.keyCode == RIGHT_ARROW_KEY || event.which == RIGHT_ARROW_KEY) {
+            		console.log('righty!');
+            	}
             };
         };
 
-        this.showTab = function() {
-
-             var targetTabId = event.target.attributes['ui-tab-nav'].value;
-             var targetTabNav = document.querySelectorAll('[ui-tab-nav="' + targetTabId + '"]')[0];
-             var targetTab = document.querySelectorAll('[ui-tab="' + targetTabId + '"]')[0];
-
-             this.hideTabs();
-
-             settings.activeTab = targetTab;
-
-             targetTabNav.className += ' tab-nav__item--current';
-             targetTab.className += ' tab--current';
-
+        this.checkTarget = function(event) {
+        	var target = event.target || event.toElement;
+                
+            if (target.parentNode.classList.contains(settings.tabsHook)){ 
+            	self.prepareTab();
+            } else {
+            	return;
+            }
         };
 
-        this.hideTabs = function(){
+        this.prepareTab = function() {
 
-            for (var i = 0, max = domElements.tab.length; i < max; i++) {
-                domElements.tab[i].className = domElements.tab[i].className.replace( /(?:^|\s)tab--current(?!\S)/g , '' );
+             var url   			= event.target.href;
+             var targetTabNavId = url.substring(url.indexOf('#') + 1);
+             var targetTabPane  = document.getElementById(targetTabNavId);
 
-                domElements.tabNav[i].className = domElements.tabNav[i].className.replace( /(?:^|\s)tab-nav__item--current(?!\S)/g , '' );
-            }
+             this.hideTab();
+
+             settings.activeTabNav  = event.target;
+             settings.activeTabPane = targetTabPane;
+
+             this.showTab();       
+        };
+
+        this.showTab = function(){
+        	settings.activeTabNav.className  += (' ' + settings.activeTabNavClassName);
+            settings.activeTabPane.className += (' ' + settings.activeTabPaneClassName);
+
+            settings.activeTabNav.setAttribute('aria-selected', 'true');
+			settings.activeTabPane.setAttribute('aria-hidden', 'false');
+			settings.activeTabNav.focus();
+        };
+
+        this.hideTab = function(){
+
+			settings.activeTabNav.setAttribute('aria-selected', 'false');
+			settings.activeTabPane.setAttribute('aria-hidden', 'true');
+            
+            settings.activeTabNav.classList.remove(settings.activeTabNavClassName);
+            settings.activeTabPane.classList.remove(settings.activeTabPaneClassName);
+
         };
     };
 
-    var tabs = new wshVanillaTabs();
+    var tabs = new wshVanillaTabs('tab-nav-1', 'tab-pane-1');
     tabs.init();
 
 })();
